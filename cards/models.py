2708b2daf041888ffca1c4b02c8108ecdfba524e
williamdev20 <williamalvs45@gmail.com>
2025-11-23 14:22:03 -0300

    Criando a tabela de subcategorias

--- cards/models.py
+++ cards/models.py
@@ -1,11 +1,54 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+
+User = get_user_model()
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, blank=False, null=False, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, blank=False, null=False)
+    slug = models.SlugField(blank=False, null=False)
 
     class Meta:
+        unique_together = [ # Um usuário não pode ter duas categorias com o mesmo nome, mas usuários diferentes sim
+            ("user", "name"),
+            ("user", "slug")
+        ] 
         verbose_name = "Category"
         verbose_name_plural = "Categories"
 
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
     def __str__(self):
         return f"Category: {self.name}"
+    
+
+
+# Falta rodar as migrações dessa tabela
+class Subcategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, blank=False, null=False)
+    slug = models.SlugField(blank=False, null=False)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        unique_together = [
+            ("category", "name"),
+            ("category", "slug")
+        ]
+        verbose_name = "Subcategory"
+        verbose_name_plural = "Subcategories"
+
+    def __str__(self):
+        return f"Subcategory: {self.name}"
+
+        
--- cards/views.py
+++ cards/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.template.loader import render_to_string
+from django.http import Http404, HttpResponse
+from django.core.exceptions import PermissionDenied
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.contrib.auth.models import User
-from .models import Category
+from .models import Category, Subcategory
 
 
 @login_required(login_url="/login/")
@@ -15,7 +17,8 @@ def create_card(request):
 def create_and_show_category(request, username):
     error = ""
 
-    if not User.objects.filter(username=username).exists():
+    procfile_user = User.objects.filter(username=username).first()
+    if procfile_user is None:
         raise Http404()
 
     if request.method == "POST":
@@ -23,13 +26,23 @@ def create_and_show_category(request, username):
 
         try:
             Category.objects.create(
+                user=request.user,
                 name=category_name
             )
         except IntegrityError as e:
             error = "Já existe uma categoria com esse nome!"
             print(f"ERROR: Erro ao tentar criar uma categoria: {e}")
 
-    categories = Category.objects.all()
+    categories = Category.objects.filter(user=procfile_user)
+
+    if request.headers.get("HX-Request"): # Requisão do HTMX
+        html = render_to_string(
+                "cards/partials/category_list.html",
+                {"categories": categories},
+                request=request
+        )
+        return HttpResponse(html)
+
 
     return render(request, "cards/show_category.html", {
         "error": error,
@@ -38,8 +51,46 @@ def create_and_show_category(request, username):
 
 
 @login_required(login_url="/login/")
-def create_subcategory_or_cards(request):
-    return render(request, "cards/create_subcategory_or_cards.html")
+def create_subcategory_or_cards(request, username, category_slug):
+    error = ""
+
+    user = User.objects.filter(username=username).first()
+    if user is None:
+        raise Http404()
+
+    if request.user.username != username:
+        raise PermissionDenied
+
+    category = get_object_or_404(Category, user=request.user, slug=category_slug)
+    
+
+    if request.method == "POST":
+        subcategory_name = request.POST.get("create_subcategory")
+
+        try:
+            Subcategory.objects.create(
+                category=category,
+                name=subcategory_name
+            )
+        except IntegrityError as e:
+            error = "Já existe uma subcategoria com esse nome!"
+            print(f"ERROR: Erro ao criar nova subcategoria: {e}")
+            
+    subcategories = Subcategory.objects.filter(category=category)
+
+    if request.headers.get("HX-Request"):
+        html = render_to_string(
+            "cards/partials/subcategories_list.html",
+            {"subcategories": subcategories},
+            request=request
+        )
+        return HttpResponse(html)
+
+    return render(request, "cards/create_subcategory_or_cards.html", {
+        "category": category,
+        "error": error,
+        "subcategories": subcategories
+    })
 
 
 @login_required(login_url="/login/")
